166695edaec7342b2f81cac1a6b682f5c815d515
poopoo888888 <howtomakeaturn@hotmail.com>
2014-01-24 15:31:42 +0800

    adding samples on welcome page

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,6 +4,13 @@
 	<meta charset="utf-8">
 	<title>Welcome to CodeIgniter</title>
 
+    <link rel="stylesheet" href="assets/css/font-awesome.min.css">
+
+
+		<link rel="stylesheet" type="text/css" media="screen" href="assets/css/bootstrap.min.css">
+    <script type="text/javascript" src="assets/js/jquery-1.10.2.min.js"></script> 
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>  
+
 	<style type="text/css">
 
 	::selection{ background-color: #E13300; color: white; }
@@ -83,6 +90,31 @@
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds</p>
 </div>
+      <a class="btn btn-lg btn-success" href="#">
+        <i class="fa fa-flag fa-2x pull-left"></i> Font Awesome<br>Work well here</a>
+
+      <div class="btn-group">
+        <a class="btn btn-primary" href="#"><i class="fa fa-user fa-fw"></i> User</a>
+        <a class="btn btn-primary dropdown-toggle" data-toggle="dropdown" href="#">
+          <span class="fa fa-caret-down"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="#"><i class="fa fa-pencil fa-fw"></i> Edit</a></li>
+          <li><a href="#"><i class="fa fa-trash-o fa-fw"></i> Delete</a></li>
+          <li><a href="#"><i class="fa fa-ban fa-fw"></i> Ban</a></li>
+          <li class="divider"></li>
+          <li><a href="#"><i class="i"></i> Bootstrap js work well here</a></li>
+        </ul>
+      </div>    
+
+      <div class='alert alert-info'><p><i class="fa fa-camera-retro fa-3x"></i> Bootstrap css work well here</p></div>
+      <div id='message' class='alert alert-success'>jquery work well here!</div>
+      <button id='jq' class='btn btn-warning'>Click Me!</button>
+      <script>
+          $('#message').hide();
+          $('#jq').click(function(){
+              $('#message').fadeIn();
+          })
+      </script>
 
 </body>
 </html>
